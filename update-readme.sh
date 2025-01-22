c36de2584d77d8ffee6e8f8877659253d5e55156
Jan Malakhovski <oxij@oxij.org>
2025-01-22 11:30:00 +0000

    update-readme.sh: simplify

--- update-readme.sh
+++ update-readme.sh
@@ -1,13 +1,17 @@
 #!/bin/sh -e
 
-sed -n "0,/# Usage/ p" README.md > README.new
-echo >> README.new
-python3 -m hoardy_adb.__main__ --help --markdown | sed '
+{
+    sed -n "/# What is/,/# Usage/ p" README.md
+    echo
+
+    python3 -m hoardy_adb.__main__ --help --markdown | sed '
 s/^\(#\+\) /#\1 /
 s/^\(#\+\) \(hoardy-adb[^A-Z[({]*\) [A-Z[({].*/\1 \2/
-' >> README.new
-./test-cli.sh --help | sed '
+'
+
+    ./test-cli.sh --help | sed '
 s/^# usage: \(.*\)$/# Development: `\1`/
-' >> README.new
+'
+} > README.new
 mv README.new README.md
 pandoc -s -V pagetitle=README -f markdown -t html README.md > README.html
